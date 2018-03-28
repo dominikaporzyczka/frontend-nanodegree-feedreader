bb049e0ba97dfb367fc86d56a974fe78ec80ba61
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-03-28 11:09:21 +0200

    test: Add error handling in test for init entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,22 +83,26 @@ $(function () {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        beforeEach(function(done) {
-            loadFeed(0, function() {
-                done();
-            });
+        beforeEach(function (done) {
+            try {
+                loadFeed(0, function () {
+                    done();
+                });
+            } catch (e) {
+                done.fail(e);
+            }
         });
 
-        it('completed its work', function(done) {
+        it('completed its work', function (done) {
             expect($('.feed').children().length > 0).toBe(true);
             done();
         });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+    
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
 }());
